740504196bd975afc5569ca801d5ac3bba04b0eb
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-06-10 17:36:10 -0400

    Add html,css,js

--- assets/js/slink.js
+++ assets/js/slink.js
@@ -47,36 +47,132 @@
 
 
 
-// ANOTHER TEST //
+//  CATLIN //
 
-$(function(){
-  'use strict';
+// $(function(){
+//   'use strict';
+// });
+//
+// var x = 1;
+// console.log(x);
+//
+// //User Login --> Show Bookmarks
+//
+// $(function () {
+// 	  $(‘#usernameLogin’).on('submit', function(e) {
+// 	    e.preventDefault();
+//
+//
+// 	    var seeBookmarkInfo = $('input[id="usernameLogin”]').val();
+//
+// //See Bookmarks
+//
+// $.getJSON('https://api.place...../users' + saveBookmark)
+//         .done(seeBookmark)
+//         .fail(showError);
+//
+//       function seeBookmark(e) {
+//         show('see_bookmark', e); //Will have to add CONFIRMATION MODAL to HTML
+//       }
+//       function show (template, model){
+//         var fn = _.template($('#' + template).html(), {
+//               variable: 'm'});
+//         $('.seeBookmarksList').html(fn(model));
+//         }
+//       )};
+//
+//
+//       //  David
+//       $(document).ready(function() {
+// //
+// //
+// //             $(function() {
+// //
+// //                     $('.user-form).on('submit', function (e) {
+// //                         e.preventDefault();
+// //                         bookMarkk();
+// //                     )};
+// //
+// //
+// //                 // var ghLogin = $('input[name="slink-login"]').val();
+// //
+// //
+// //                 function bookMarkk() {
+// //                     $.ajax({
+// //                             url: "/Users/davidgrayboff/Documents/dev_projects/TIY/KDR-Slink copy/APIS/Slink/users/user-1/test.json",
+// //                             type: 'GET',
+// //                             dataType: 'text',
+// //                             success: successFunction,
+// //                             error: errorFunction,
+// //
+// //                             complete: $('.alert').append('.info');
+// //                         }
+// //                     });
+// //             }
+// //
+// //           });
+//
+// // ajaxComplete //
+//
+// $(function() {
+//   "use strict";
+//
+//  function jSONson() {
+//   $.ajax({
+//     url: "http://localhost:3000/0",
+//     dataType: "JSON",
+//     method: 'GET',
+//     success: function(data)
+//     {
+//       console.log("success!");
+//       console.log(data);
+//       // var a = data.a;
+//       $('.infoTitle').text(data.title);
+//       $('.infoDesc').text(data.description);
+//       $('.infoURL').text(data.url);
+//       $('.infoBy').text(data.created_by);
+//       $('.infoAt').text(data.created_at);
+//     }
+//     // success: successFunction
+//   });
+//   console.log("Something works");
+//
+//
+// }
+//
+//
+// $('.user-form').on('submit', function(e) {
+//   e.preventDefault();
+//   jSONson();
+// })
+// })
+//////////////////////////////////////////////////////////////////
+//                   RHONDA
+$(".Bookmark-choices").click(function(){
+    $(this).next().toggle();
+    $("i", this).toggleClass("hide-right-arrow");
+    $("i", this).last().toggleClass("show-down-arrow");
+  });
+
+
+
+  $(".Section-A").click(function(){
+      $(this).next().toggle();
+      $("i", this).toggleClass("hide-right-arrow");
+      $("i", this).last().toggleClass("show-down-arrow");
+  });
+
+
+////////////       MODAL                
+    $('h1').on("click", function () {//when you click on the btn the modal pops up//
+        $(this).text('Make-Recommendations');//when you click on the btn the text changes to Hi//
+
+    $('.modal-container').addClass('showing');
+
+    $('.modal-close, .modal-container').on('click', function () {//when you click on the X the modal container disappears//
+      $('.modal-container').removeClass('showing');
+    });
+    // $('.modal').click(function (e) // stops the modal container from appearing after it's closed//
+    //   e.stopPropagation();
+
 });
-
-var x = 1;
-console.log(x);
-
-//User Login --> Show Bookmarks
-
-$(function () {
-	  $(‘#usernameLogin’).on('submit', function(e) {
-	    e.preventDefault();
-
-
-	    var seeBookmarkInfo = $('input[id="usernameLogin”]').val();
-
-//See Bookmarks
-
-$.getJSON('https://api.place...../users' + saveBookmark)
-        .done(seeBookmark)
-        .fail(showError);
-
-      function seeBookmark(e) {
-        show('see_bookmark', e); //Will have to add CONFIRMATION MODAL to HTML
-      }
-      function show (template, model){
-        var fn = _.template($('#' + template).html(), {
-              variable: 'm'});
-        $('.seeBookmarksList').html(fn(model));
-        }
-      )};
